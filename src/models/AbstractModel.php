fc52050540c882c8bf104efac814c26a003d06ae
Albert Borsos <albertborsos@me.com>
2019-06-20 15:30:17 +0200

    remove deprecated classes and gii templates

--- src/models/AbstractModel.php
+++ src/models/AbstractModel.php
@@ -17,10 +17,8 @@ use yii\helpers\ArrayHelper;
  * Class AbstractModel
  * @package albertborsos\ddd\models
  */
-abstract class AbstractModel extends Component implements IteratorAggregate, ArrayAccess, Arrayable
+abstract class AbstractModel extends Component
 {
-    use ArrayableTrait;
-
     /**
      * The ID of the (main) object
      * @var integer|mixed
@@ -144,81 +142,5 @@ abstract class AbstractModel extends Component implements IteratorAggregate, Arr
     public function hasErrors()
     {
         return !empty($this->getErrors());
-    }
-
-
-    /**
-     * Retrieve an external iterator
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return ArrayIterator|\Traversable An instance of an object implementing <b>Iterator</b> or
-     * <b>Traversable</b>
-     * @since 5.0.0
-     */
-    public function getIterator()
-    {
-        $attributes = $this->getAttributes();
-        return new ArrayIterator($attributes);
-    }
-
-    /**
-     * Whether a offset exists
-     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
-     * @param mixed $offset <p>
-     * An offset to check for.
-     * </p>
-     * @return boolean true on success or false on failure.
-     * </p>
-     * <p>
-     * The return value will be casted to boolean if non-boolean was returned.
-     * @since 5.0.0
-     */
-    public function offsetExists($offset)
-    {
-        return isset($this->$offset);
-    }
-
-    /**
-     * Offset to retrieve
-     * @link http://php.net/manual/en/arrayaccess.offsetget.php
-     * @param mixed $offset <p>
-     * The offset to retrieve.
-     * </p>
-     * @return mixed Can return all value types.
-     * @since 5.0.0
-     */
-    public function offsetGet($offset)
-    {
-        return $this->$offset;
-    }
-
-    /**
-     * Offset to set
-     * @link http://php.net/manual/en/arrayaccess.offsetset.php
-     * @param mixed $offset <p>
-     * The offset to assign the value to.
-     * </p>
-     * @param mixed $value <p>
-     * The value to set.
-     * </p>
-     * @return void
-     * @since 5.0.0
-     */
-    public function offsetSet($offset, $value)
-    {
-        $this->$offset = $value;
-    }
-
-    /**
-     * Offset to unset
-     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
-     * @param mixed $offset <p>
-     * The offset to unset.
-     * </p>
-     * @return void
-     * @since 5.0.0
-     */
-    public function offsetUnset($offset)
-    {
-        $this->$offset = null;
     }
 }
--- src/models/AbstractService.php
+++ src/models/AbstractService.php
@@ -24,34 +24,4 @@ abstract class AbstractService extends AbstractModel
     {
         return $this->getForm()->getAttributes();
     }
-
-    /**
-     * Converts the model into an array.
-     *
-     * This method will first identify which fields to be included in the resulting array by calling [[resolveFields()]].
-     * It will then turn the model into an array with these fields. If `$recursive` is true,
-     * any embedded objects will also be converted into arrays.
-     *
-     * If the model implements the [[Linkable]] interface, the resulting array will also have a `_link` element
-     * which refers to a list of links as specified by the interface.
-     *
-     * @param array $fields the fields being requested. If empty, all fields as specified by [[fields()]] will be returned.
-     * @param array $expand the additional fields being requested for exporting. Only fields declared in [[extraFields()]]
-     * will be considered.
-     * @param bool $recursive whether to recursively return array representation of embedded objects.
-     * @return array the array representation of the object
-     */
-    public function toArray(array $fields = [], array $expand = [], $recursive = true)
-    {
-        $data = [];
-        foreach ($this->resolveFields($fields, $expand) as $field => $definition) {
-            $data[$field] = is_string($definition) ? $this->getForm()->$definition : call_user_func($definition, $this, $field);
-        }
-
-        if ($this instanceof Linkable) {
-            $data['_links'] = Link::serialize($this->getLinks());
-        }
-
-        return $recursive ? ArrayHelper::toArray($data) : $data;
-    }
 }
