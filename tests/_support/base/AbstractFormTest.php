692a5835102507a64d47b81014f11c9b0a44a57e
Albert Borsos <albertborsos@me.com>
2019-08-01 17:07:40 +0200

    update abstract test base classes
    replace business objects with entity
    mark model related field and method as depreaceted

--- tests/_support/base/AbstractFormTest.php
+++ tests/_support/base/AbstractFormTest.php
@@ -2,26 +2,41 @@
 
 namespace albertborsos\ddd\tests\support\base;
 
+use albertborsos\ddd\interfaces\EntityInterface;
 use Yii;
 
 abstract class AbstractFormTest extends Unit
 {
+    /**
+     * @var string
+     */
     protected $formClass;
+
+    /**
+     * @var string
+     * @deprecated since 1.1.0 will be removed in 2.0.0. Use `entityClass` property instead of `modelClass`.
+     */
     protected $modelClass;
 
     /**
+     * @var string
+     * @since 1.1.0
+     */
+    protected $entityClass;
+
+    /**
      * @param array $loadParams
-     * @param \albertborsos\ddd\interfaces\BusinessObject|null $businessObject
+     * @param \albertborsos\ddd\interfaces\BusinessObject|null $entity
      * @param array $initParams
      * @param bool $isInitParamsAreArguments
      * @return \albertborsos\ddd\interfaces\FormObject|\yii\base\Model
      */
-    protected function mockForm($loadParams = [], \albertborsos\ddd\interfaces\BusinessObject $businessObject = null, $initParams = [], $isInitParamsAreArguments = false)
+    protected function mockForm($loadParams = [], \albertborsos\ddd\interfaces\EntityInterface $entity = null, $initParams = [], $isInitParamsAreArguments = false)
     {
         if ($isInitParamsAreArguments) {
-            $params = $businessObject ? array_merge([$businessObject], $initParams) : $initParams;
+            $params = $entity ? array_merge([$entity], $initParams) : $initParams;
         } else {
-            $params = $businessObject ? [$businessObject, $initParams] : [$initParams];
+            $params = $entity ? [$entity, $initParams] : [$initParams];
         }
 
         /** @var \yii\base\Model|\albertborsos\ddd\interfaces\FormObject $form */
@@ -32,8 +47,21 @@ abstract class AbstractFormTest extends Unit
     }
 
     /**
+     * @param $config
+     * @return EntityInterface
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function mockEntity($config)
+    {
+        $entityClass = $this->entityClass ?? $this->modelClass;
+
+        return Yii::createObject($entityClass, [$config]);
+    }
+
+    /**
      * @param $id
      * @return \yii\db\ActiveRecord
+     * @deprecated since 1.1.0 will be removed in 2.0.0
      */
     protected function getModel($id)
     {
--- tests/_support/base/AbstractServiceTest.php
+++ tests/_support/base/AbstractServiceTest.php
@@ -4,49 +4,20 @@ namespace albertborsos\ddd\tests\support\base;
 
 use Yii;
 
-abstract class AbstractServiceTest extends Unit
+abstract class AbstractServiceTest extends AbstractFormTest
 {
-    protected $formClass;
-    protected $serviceClass;
-    protected $modelClass;
-
     /**
-     * @param array $loadParams
-     * @param \albertborsos\ddd\interfaces\BusinessObject|null $businessObject
-     * @param array $initParams
-     * @return \albertborsos\ddd\interfaces\FormObject|\yii\base\Model
+     * @var string
      */
-    protected function mockForm($loadParams = [], \albertborsos\ddd\interfaces\BusinessObject $businessObject = null, $initParams = [], $isInitParamsAreArguments = false)
-    {
-        if ($isInitParamsAreArguments) {
-            $params = $businessObject ? array_merge([$businessObject], $initParams) : $initParams;
-        } else {
-            $params = $businessObject ? [$businessObject, $initParams] : [$initParams];
-        }
-
-        /** @var \yii\base\Model|\albertborsos\ddd\interfaces\FormObject $form */
-        $form = Yii::createObject($this->formClass, $params);
-        $form->load($loadParams, '');
-
-        return $form;
-    }
+    protected $serviceClass;
 
     /**
      * @param \albertborsos\ddd\interfaces\FormObject $formObject
-     * @param \albertborsos\ddd\interfaces\BusinessObject|null $businessObject
+     * @param \albertborsos\ddd\interfaces\BusinessObject|null $entity
      * @return \albertborsos\ddd\models\AbstractService
      */
-    protected function mockService(\albertborsos\ddd\interfaces\FormObject $formObject, \albertborsos\ddd\interfaces\BusinessObject $businessObject = null)
+    protected function mockService(\albertborsos\ddd\interfaces\FormObject $formObject, \albertborsos\ddd\interfaces\EntityInterface $entity = null)
     {
-        return Yii::createObject($this->serviceClass, [$formObject, $businessObject]);
-    }
-
-    /**
-     * @param $id
-     * @return \yii\db\ActiveRecord
-     */
-    protected function getModel($id)
-    {
-        return call_user_func([$this->modelClass, 'findOne'], $id);
+        return Yii::createObject($this->serviceClass, [$formObject, $entity]);
     }
 }
