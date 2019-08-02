592a21683ddb9228eaa45f3bd04643320187d210
Albert Borsos <albertborsos@me.com>
2019-08-02 11:42:01 +0200

    add repository to AbstractServiceTest

--- tests/_support/base/AbstractServiceTest.php
+++ tests/_support/base/AbstractServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace albertborsos\ddd\tests\support\base;
 
+use albertborsos\ddd\interfaces\RepositoryInterface;
 use Yii;
 
 abstract class AbstractServiceTest extends AbstractFormTest
@@ -12,6 +13,11 @@ abstract class AbstractServiceTest extends AbstractFormTest
     protected $serviceClass;
 
     /**
+     * @var string
+     */
+    protected $repositoryInterface;
+
+    /**
      * @param \albertborsos\ddd\interfaces\FormObject $formObject
      * @param \albertborsos\ddd\interfaces\BusinessObject|null $entity
      * @return \albertborsos\ddd\models\AbstractService
@@ -19,5 +25,14 @@ abstract class AbstractServiceTest extends AbstractFormTest
     protected function mockService(\albertborsos\ddd\interfaces\FormObject $formObject, \albertborsos\ddd\interfaces\EntityInterface $entity = null)
     {
         return Yii::createObject($this->serviceClass, [$formObject, $entity]);
+    }
+
+    /**
+     * @return RepositoryInterface
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function getRepository()
+    {
+        return Yii::createObject($this->repositoryInterface);
     }
 }
